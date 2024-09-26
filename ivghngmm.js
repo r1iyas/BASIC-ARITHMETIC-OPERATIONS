a49c173a12da424d22d1e140b125eeac69cfea71
r1iyas <riyasma97@gmail.com>
2024-09-26 23:45:46 +0530

    basic-arithmetic-operations

---
+++ ivghngmm.js
@@ -0,0 +1,24 @@
+/*sum*/
+var a=10;
+var b=5;
+console.log(a+b);
+
+/*quotient*/
+let c=10;
+let d=5;
+console.log(c/d);
+
+/*diffrence*/
+let e=10;
+let f=5;
+console.log(e-f);
+
+/*product*/
+let g=10;
+let h=5;
+console.log(g*h);
+
+
+
+
+
